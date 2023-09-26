2fde87542b9503958cb68734ec1c3787d01bf11a
felipe-coletti <felipecoletti2@gmail.com>
2023-09-26 09:28:52 -0300

    Criação das telas de cadastro e login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Patient from "../pages/Patient";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
 
 const PagesRoutes = () => {
     return (
@@ -8,6 +10,8 @@ const PagesRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/patient/:slug" element={<Patient/>}/>
+                <Route path="/signin" element={<Signin/>}/>
+                <Route path="/signup" element={<Signup/>}/>
             </Routes>
         </Router>
     )
