0cb64bfb268e00c7413797ebc0b41597ff1de2fd
felipe-coletti <felipecoletti2@gmail.com>
2023-10-10 18:38:14 -0300

    Ajuste na consulta de pacientes

--- src/pages/Patient/index.js
+++ src/pages/Patient/index.js
@@ -1,9 +1,29 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import { useParams } from "react-router-dom";
-import patients from "../../util";
+//import patients from "../../util/Patients";
 
 const Patient = () => {
+    const [patients, setPatients] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        const consult = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/pacientes")
+                if (!response.ok) {
+                    throw new Error()
+                }
+                const data = await response.json()
+                console.log(JSON.stringify(data))
+                setPatients(data)
+            } catch (error) {
+                setErrorMessage(error.message)
+            }
+        }
+        consult()
+    }, [])
+
     const {slug} = useParams();
 
     const patient = patients.find(item => item.slug === slug);
