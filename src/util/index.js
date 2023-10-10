f7be1cf1fd526e607952a9a19951713e9433ed5f
felipe-coletti <felipecoletti2@gmail.com>
2023-10-10 16:55:26 -0300

    Conexão com a API

--- src/util/index.js
+++ src/util/index.js
@@ -1,12 +1,46 @@
-const patients = [
+/*const patients = [
     {
         slug: '0',
-        name: 'João'
+        name: 'João',
+        cpf: '111.111.111-11',
+        birthDate: '',
+        email: '',
+        password: '',
+        address: ''
     },
     {
         slug: '1',
-        name: 'Maria'
+        name: 'Maria',
+        cpf: '222.222.222-22',
+        birthDate: '',
+        email: '',
+        password: '',
+        address: ''
     }
-]
+]*/
 
-export default patients
+import { useState, useEffect } from "react";
+
+const Patients = () => {
+    const [patients, setPatients] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        const consult = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/pacientes")
+                if (!response.ok) {
+                    throw new Error()
+                }
+                const data = await response.json()
+                console.log(JSON.stringify(data))
+                setPatients(data)
+            } catch (error) {
+                setErrorMessage(error.message)
+            }
+        }
+        consult()
+    }, [])
+}
+
+export default Patients
