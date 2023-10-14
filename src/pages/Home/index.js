86c6e8e2544426004e9c177a42440c546d44705b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-10-14 16:47:24 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import { React, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
-//import patients from "../../util/Patients";
 
 const Home = () => {
     const [patients, setPatients] = useState([]);
