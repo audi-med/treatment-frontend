c9193609828bfb425b752363bc74decb91170711
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-10-17 09:44:29 -0300

    Update index.js

--- src/pages/RegisterPatient/index.js
+++ src/pages/RegisterPatient/index.js
@@ -27,7 +27,7 @@ const RegisterPatient = () => {
         }
 
         try {
-            const response = await fetch("http://localhost:8080/api/v1/pacientes", {
+            const response = await fetch("http://localhost:8080/api/v1/pacientes/cadastro", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
