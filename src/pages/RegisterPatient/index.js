1777ff07a3dece602c03c908a879ceb06e450b71
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-10-17 08:43:17 -0300

    Update index.js

--- src/pages/RegisterPatient/index.js
+++ src/pages/RegisterPatient/index.js
@@ -21,7 +21,9 @@ const RegisterPatient = () => {
             numeroDeTelefone,
             endereco,
             email,
-            senha
+            senha,
+            nomeDoResponsavel,
+            cpfDoResponsavel
         }
 
         try {
