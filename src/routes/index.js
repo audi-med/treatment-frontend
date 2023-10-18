e9720ddc6f1a3120b21bceebda915ab65ece140c
felipe-coletti <felipecoletti2@gmail.com>
2023-10-18 10:59:46 -0300

    Transformação da tela de cadastrar paciente em um modal

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import ViewPatients from "../pages/ViewPatients";
 import ViewPatient from "../pages/ViewPatient";
 import Signin from "../pages/Signin";
-import ChangePatient from "../pages/ChangePatient";
 
 const PagesRoutes = () => {
     return (
@@ -11,7 +10,6 @@ const PagesRoutes = () => {
                 <Route path="/signin" element={<Signin/>}/>
                 <Route path="/" element={<ViewPatients/>}/>
                 <Route path="/patient/:id" element={<ViewPatient/>}/>
-                <Route path="/change-patient" element={<ChangePatient/>}/>
             </Routes>
         </Router>
     )
