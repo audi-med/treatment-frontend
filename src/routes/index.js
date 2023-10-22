e2a59267afc22d23eae2d25227e8a47340532971
felipe-coletti <felipecoletti2@gmail.com>
2023-10-22 16:15:02 -0300

    Criação da rota da landing page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from "../pages/Home";
 import Signin from "../pages/Signin";
 import ViewPatients from "../pages/ViewPatients";
 import ViewPatient from "../pages/ViewPatient";
@@ -7,6 +8,7 @@ const PagesRoutes = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/signin" element={<Signin/>}/>
                 <Route path="/patients" element={<ViewPatients/>}/>
                 <Route path="/patients/:id" element={<ViewPatient/>}/>
