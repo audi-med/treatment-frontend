a2f5e9934cddd2507c859aa074ea384ba404c2c2
felipe-coletti <felipecoletti2@gmail.com>
2023-10-22 16:14:07 -0300

    Criação do footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import { React, useState } from "react";
+import styles from "./styles.module.css";
+
+const Footer = () => {
+    <footer className={styles.container}>
+        
+    </footer>
+}
+
+export default Footer
