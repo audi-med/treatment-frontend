134d0f4d26d126c7c4f114b46ec9ea36ed0ef7fd
felipe-coletti <felipecoletti2@gmail.com>
2023-10-22 18:28:31 -0300

    Estruturação da landing page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,48 @@
 import React from "react";
 import styles from "./styles.module.css";
+import { Icon } from '@iconify/react';
 
 const Home = () => {
-    <div className={styles.container}>
-        <section className={styles.section}>
-            <div>
-                <h1>
-                </h1>
-                <p>
-                </p>
+    return (
+        <div className={styles.container}>
+            <section className={styles.section}>
+                <h1></h1>
+                <p className={styles.paragraph}></p>
                 <button className={styles.primaryButton}>Entrar</button>
-            </div>
-        </section>
-        <section className={styles.section}>
-            <div>
-                <h2>
-                </h2>
-                <p>
-                </p>
-            </div>
-            <div>
-                <h2>
-                </h2>
-                <p>
-                </p>
-            </div>
-            <div>
-                <h2>
-                </h2>
-                <p>
-                </p>
-            </div>
-        </section>
-        <section className={styles.section}>
-            <div>
-                <h2>
-                </h2>
-                <p>
-                </p>
-            </div>
-        </section>
-    </div>
+            </section>
+            <section className={styles.section}>
+                <div className={styles.grid}>
+                    <div className={styles.contentArea}>
+                        <h2></h2>
+                        <p className={styles.paragraph}></p>
+                    </div>
+                    <div className={styles.contentArea}>
+                        <h2></h2>
+                        <p className={styles.paragraph}></p>
+                    </div>
+                    <div className={styles.contentArea}>
+                        <h2></h2>
+                        <p className={styles.paragraph}></p>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.section}>
+                <h2>Licenças</h2>
+                <div className={styles.grid}>
+                    <div className={styles.card}>
+                        <h3>Licença gratuita</h3>
+                        <p className={styles.paragraph}><span className={styles.highlightText}>R$ 0,00</span>/mês</p>
+                        <ul className={styles.benefitsList}>
+                            <li className={styles.benefit}>
+                                <span className={styles.benefitIcon}><Icon icon="ic:outline-check" /></span><p className={styles.paragraph}>Acesso a todos os recursos básicos.</p>
+                            </li>
+                        </ul>
+                        <button className={styles.primaryButton}>Entrar</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Home
