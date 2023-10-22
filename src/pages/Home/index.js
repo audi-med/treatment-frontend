e9774d5e32672db34a29b7d69d6a40ba2ef5e28e
felipe-coletti <felipecoletti2@gmail.com>
2023-10-22 16:14:36 -0300

    Criação da landing page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Home = () => {
+    <div className={styles.container}>
+        <section className={styles.section}>
+            <div>
+                <h1>
+                </h1>
+                <p>
+                </p>
+                <button className={styles.primaryButton}>Entrar</button>
+            </div>
+        </section>
+        <section className={styles.section}>
+            <div>
+                <h2>
+                </h2>
+                <p>
+                </p>
+            </div>
+            <div>
+                <h2>
+                </h2>
+                <p>
+                </p>
+            </div>
+            <div>
+                <h2>
+                </h2>
+                <p>
+                </p>
+            </div>
+        </section>
+        <section className={styles.section}>
+            <div>
+                <h2>
+                </h2>
+                <p>
+                </p>
+            </div>
+        </section>
+    </div>
+}
+
+export default Home
