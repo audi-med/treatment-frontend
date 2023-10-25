12753db506a8e384599610160062cb7040893a66
felipe-coletti <felipecoletti2@gmail.com>
2023-10-25 11:07:04 -0300

    Função para mostrar os dados já obtidos.

--- src/components/EditPatient/index.js
+++ src/components/EditPatient/index.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import { Icon } from '@iconify/react';
 
@@ -12,6 +12,33 @@ const EditPatient = ({ id, onClose }) => {
     const closeByButton = () => {
         onClose();
     }
+
+    const [errorMessage, setErrorMessage] = useState(null);
+    
+    useEffect(() => {
+        const consult = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/pacientes/" + id)
+                if (!response.ok) {
+                    throw new Error()
+                }
+                const data = await response.json()
+                console.log(JSON.stringify(data))
+                setNome(data.nome || "");
+                setCPF(data.cpf || "");
+                setDataDeNascimento(data.dataDeNascimento || "");
+                setNumeroDeTelefone(data.numeroDeTelefone || "");
+                setEndereco(data.endereco || "");
+                setEmail(data.email || "");
+                setSenha(data.senha || "");
+                setNomeDoResponsavel(data.nomeDoResponsavel || "");
+                setCPFDoResponsavel(data.cpfDoResponsavel || "");
+            } catch (error) {
+                setErrorMessage(error.message)
+            }
+        }
+        consult()
+    }, [id])
 
     const [nome, setNome] = useState("");
     const [cpf, setCPF] = useState("");
@@ -35,11 +62,11 @@ const EditPatient = ({ id, onClose }) => {
             senha,
             nomeDoResponsavel,
             cpfDoResponsavel
-        }
+        };
 
         try {
-            const response = await fetch("http://localhost:8080/api/v1/pacientes/atualizar" + id, {
-                method: "POST",
+            const response = await fetch("http://localhost:8080/api/v1/pacientes/atualizar/" + id, {
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
                 },
@@ -52,7 +79,7 @@ const EditPatient = ({ id, onClose }) => {
         } catch (error) {
             setMessage("Erro ao editar o paciente. Verifique os dados informados.")
         }
-    
+
         onClose();
     };
 
@@ -72,39 +99,39 @@ const EditPatient = ({ id, onClose }) => {
                         <form className={styles.form}>
                             <div className={styles.inputField}>
                                 <label htmlFor="name-input">Nome completo</label>
-                                <input className={styles.input} onChange={(e) => setNome(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setNome(e.target.value)} value={nome} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="cpf-input">CPF</label>
-                                <input className={styles.input} onChange={(e) => setCPF(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setCPF(e.target.value)} value={cpf} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="birthdate-input">Data de nascimento</label>
-                                <input className={styles.input} onChange={(e) => setDataDeNascimento(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setDataDeNascimento(e.target.value)} value={dataDeNascimento} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="phone-number-input">Número de telefone</label>
-                                <input className={styles.input} onChange={(e) => setNumeroDeTelefone(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setNumeroDeTelefone(e.target.value)} value={numeroDeTelefone} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="address-input">Endereço</label>
-                                <input className={styles.input} onChange={(e) => setEndereco(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setEndereco(e.target.value)} value={endereco} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="email-input">E-mail</label>
-                                <input className={styles.input} onChange={(e) => setEmail(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setEmail(e.target.value)} value={email} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="password-input">Senha</label>
-                                <input className={styles.input} onChange={(e) => setSenha(e.target.value)} type="password" required/>
+                                <input className={styles.input} onChange={(e) => setSenha(e.target.value)} value={senha} type="password" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="responsible-name-input">Nome do responsável</label>
-                                <input className={styles.input} onChange={(e) => setNomeDoResponsavel(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setNomeDoResponsavel(e.target.value)} value={nomeDoResponsavel} type="text" required />
                             </div>
                             <div className={styles.inputField}>
                                 <label htmlFor="responsible-cpf-input">CPF do responsável</label>
-                                <input className={styles.input} onChange={(e) => setCPFDoResponsavel(e.target.value)} type="text" required/>
+                                <input className={styles.input} onChange={(e) => setCPFDoResponsavel(e.target.value)} value={cpfDoResponsavel} type="text" required />
                             </div>
                             <button className={styles.primaryButton} onClick={handleEdit} type="submit">Salvar</button>
                         </form>
