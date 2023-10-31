d93bc8dab671609915b0e33a05702d53e440bf69
felipe-coletti <felipecoletti2@gmail.com>
2023-10-31 17:02:46 -0300

    Atualização do componente modal.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,30 +3,17 @@ import styles from "./styles.module.css";
 import { Icon } from '@iconify/react';
 
 const Modal = ({ onClose, content }) => {
-    const closeByBackground = (e) => {
-        if (e.target === e.currentTarget) {
-            onClose()
-        }
-    }
-
-    const closeByButton = () => {
-        onClose()
-    }
-
-
     return (
-        <div className={styles.mainContainer}>
-            <div className={styles.shadow}></div>
-            <div className={styles.mainContentArea}>
-                <div className={styles.container} onClick={closeByBackground}>
-                    <div className={styles.contentArea}>
-                        <div className={styles.topArea}>
-                            <button className={styles.button} onClick={closeByButton}>
-                                <Icon icon="tabler:x" />
-                            </button>
-                        </div>
-                        {content}
+        <div className={styles.modalContainer}>
+            <div className={styles.container}>
+                <div className={styles.shadow} onClick={onClose}></div>
+                <div className={styles.contentArea}>
+                    <div className={styles.topArea}>
+                        <button className={styles.button} onClick={onClose}>
+                            <Icon icon="tabler:x" />
+                        </button>
                     </div>
+                    {content}
                 </div>
             </div>
         </div>
