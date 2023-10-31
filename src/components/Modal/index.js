a3606b7c55ca09ff2eafcd5b0861f20accd871d7
felipe-coletti <felipecoletti2@gmail.com>
2023-10-31 17:04:34 -0300

    Ajuste de nomenclatura em um estilo do componente modal.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -6,7 +6,7 @@ const Modal = ({ onClose, content }) => {
     return (
         <div className={styles.modalContainer}>
             <div className={styles.container}>
-                <div className={styles.shadow} onClick={onClose}></div>
+                <div className={styles.backdrop} onClick={onClose}></div>
                 <div className={styles.contentArea}>
                     <div className={styles.topArea}>
                         <button className={styles.button} onClick={onClose}>
