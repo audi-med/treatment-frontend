2c992ee0fd7c78d8b5161f00bb644a78abde83c3
felipe-coletti <felipecoletti2@gmail.com>
2023-11-07 09:35:38 -0300

    Criação do CRUD de médicos.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Signin from "../pages/Signin";
-import ViewPatients from "../pages/ViewPatients";
-import ViewPatient from "../pages/ViewPatient";
+import ViewDoctors from "../pages/ViewDoctors";
+import ViewDoctor from "../pages/ViewDoctor";
 import ViewReceptionists from "../pages/ViewReceptionists";
 import ViewReceptionist from "../pages/ViewReceptionist";
+import ViewPatients from "../pages/ViewPatients";
+import ViewPatient from "../pages/ViewPatient";
 
 const PagesRoutes = () => {
     return (
@@ -12,6 +14,8 @@ const PagesRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/signin" element={<Signin/>}/>
+                <Route path="/doctors" element={<ViewDoctors/>}/>
+                <Route path="/doctors/:id" element={<ViewDoctor/>}/>
                 <Route path="/receptionists" element={<ViewReceptionists/>}/>
                 <Route path="/receptionists/:id" element={<ViewReceptionist/>}/>
                 <Route path="/patients" element={<ViewPatients/>}/>
