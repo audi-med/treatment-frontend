2398a389a52b3676c12ab9320664a0c405f4e4f4
felipe-coletti <felipecoletti2@gmail.com>
2023-11-07 22:26:06 -0300

    Ajuste de copyright.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 const Footer = () => {
     return (
         <footer className={styles.container}>
-            <p className={styles.paragraph}>&copy; 2023 AudiTech</p>
+            <p className={styles.paragraph}>&copy; 2023 Felipe Coletti</p>
         </footer>
     )
 }
