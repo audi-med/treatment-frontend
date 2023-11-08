c20ab7ee6d79863111449a913949cfc42297470e
felipe-coletti <felipecoletti2@gmail.com>
2023-11-07 22:03:51 -0300

    Recolocação de dados na exibição.

--- src/pages/ViewDoctor/index.js
+++ src/pages/ViewDoctor/index.js
@@ -34,6 +34,7 @@ const ViewDoctor = () => {
                         <p>Nome: {doctor.nome}</p>
                         <p>CPF: {doctor.cpf}</p>
                         <p>E-mail: {doctor.email}</p>
+                        <p>Senha: {doctor.senha}</p>
                     </div>
                 ):(
                     <div className={styles.messageArea}>
