49e936be87a99f79552ef7289402ffd6de1e0589
felipe-coletti <felipecoletti2@gmail.com>
2023-11-15 16:23:01 -0300

    Create exam page

---
+++ src/pages/Exam/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Exam = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.contentArea}>
+                <h1 className={styles.primaryTitle}>Exame</h1>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Exam
