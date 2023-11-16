4500c3542d7049acc9b1b09166409714cb507432
felipe-coletti <felipecoletti2@gmail.com>
2023-11-16 10:40:03 -0300

    Audio correction

--- src/pages/Exam/index.js
+++ src/pages/Exam/index.js
@@ -1,107 +1,115 @@
-import { React, useState, useEffect } from "react";
-import styles from "./styles.module.css";
-
+import React, { useState, useEffect } from "react"
+import styles from "./styles.module.css"
+ 
 const Exam = () => {
-    const [questions, setQuestions] = useState([])
-    const [errorMessage, setErrorMessage] = useState(null)
-    const [word, setWord] = useState("")
-    const [answered, setAnswered] = useState(false)
-    const [correctAnswer, setCorrectAnswer] = useState(false)
-
-    useEffect(() => {
-        const consult = async () => {
-            try {
-                const response = await fetch("http://localhost:8080/api/v1/tratamento/audio")
-                if (!response.ok) {
-                    throw new Error()
-                }
-                const data = await response.json()
-                setQuestions(data)
-            } catch (error) {
-                setErrorMessage("Erro ao exibir o tratamento.")
-            }
+  const [audioPairs, setAudioPairs] = useState([])
+  const [currentPairIndex, setCurrentPairIndex] = useState(0)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [word, setWord] = useState("")
+  const [answered, setAnswered] = useState(false)
+  const [correctAnswer, setCorrectAnswer] = useState(false)
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/tratamento/audio")
+        if (!response.ok) {
+          throw new Error()
         }
-        consult()
-    }, [])
-
-    const handleVerify = async (response) => {
-        if (!response) {
-            setAnswered(true)
-        }
-
-        if (response.toLowerCase().replace(/\s/g, '') === questions[i].palavra) {
-            setCorrectAnswer(true)
-        } else {
-            setCorrectAnswer(false)
-        }
+        const data = await response.json()
+        setAudioPairs(data)
+      } catch (error) {
+        setErrorMessage("Erro ao exibir o tratamento.")
+      }
     }
-
-    var i = 0
-
-    const decreaseIndex = async () => {
-        if (i > 0) {
-            i -= 1
-        }
-        console.log(i)
+    fetchData()
+  }, [])
+ 
+  const handleVerify = () => {
+    if (!word) {
+      setAnswered(true)
+      return
     }
-
-    const increaseIndex = async () => {
-        if (i < questions.length - 1) {
-            i += 1
-        }
-        console.log(i)
+ 
+    const currentPair = audioPairs[currentPairIndex]
+    const isCorrect = word.toLowerCase().replace(/\s/g, "") === currentPair.palavra
+ 
+    setAnswered(true)
+    setCorrectAnswer(isCorrect)
+  }
+ 
+  const decreaseIndex = () => {
+    if (currentPairIndex > 0) {
+      setCurrentPairIndex(currentPairIndex - 1)
     }
-
-    const handleFinalize = async () => {
-        
+  }
+ 
+  const increaseIndex = () => {
+    if (currentPairIndex < audioPairs.length - 1) {
+      setCurrentPairIndex(currentPairIndex + 1)
     }
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.contentArea}>
-                <h1 className={styles.primaryTitle}>Tratamento</h1>
-                {errorMessage === null ? (
-                    <div className={styles.dataArea}>
-                        <div className={styles.audioArea}>
-                            <audio src={questions[i].audio} controls></audio>
-                            {answered && (
-                                correctAnswer ? (
-                                    <p className={styles.correctAnswer}>Resposta correta</p>
-                                ) : (
-                                    <p className={styles.incorrectAnswer}>Resposta incorreta</p>
-                                )
-                            )}
-                        </div>
-                        <div className={styles.inputField}>
-                            <label htmlFor="word-input">Digite a palavra do áudio</label>
-                            <input id="word-input" className={styles.input} onChange={(e) => setWord(e.target.value)} type="text" required/>
-                        </div>
-                        <button className={styles.primaryButton} onClick={() => handleVerify(word)}>Verificar resposta</button>
-                        <p className={styles.paragraph}>{i + 1} de {questions.length}</p>
-                        <div className={styles.buttonsArea}>
-                            {i === 0 ? (
-                                <button className={styles.secondaryButton} disabled>Anterior</button>
-                            ):(
-                                <button className={styles.secondaryButton} onClick={() => decreaseIndex()}>Anterior</button>
-                            )}
-                            {questions.length === 0 || i === questions.length - 1 || answered === false ? (
-                                <button className={styles.secondaryButton} disabled>Próximo</button>
-                            ):(
-                                <button className={styles.secondaryButton} onClick={() => increaseIndex()}>Próximo</button>
-                            )}
-                            {i === questions.length - 1 && (
-                                <button className={styles.secondaryButton} onClick={() => handleFinalize()}>Finalizar</button>
-                            )}
-                        </div>
-                    </div>
-                ):(
-                    <div className={styles.messageArea}>
-                        <p className={styles.errorMessage}>{errorMessage}</p>
-                    </div>
-                )}
+  }
+ 
+  const handleFinalize = () => {
+    // Adicione a lógica para finalizar o tratamento, se necessário
+  }
+ 
+  const playAudio = () => {
+    const currentPair = audioPairs[currentPairIndex]
+    const audio = new Audio(`data:audio/wavbase64,${currentPair.audio}`)
+    audio.play()
+  }
+ 
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentArea}>
+        <h1 className={styles.primaryTitle}>Tratamento</h1>
+        {errorMessage === null ? (
+          <div className={styles.dataArea}>
+            <div className={styles.audioArea}>
+              {audioPairs.length > 0 && (
+                <div>
+                  <button className={styles.secondaryButton} onClick={playAudio}>Reproduzir Áudio</button>
+                  {answered && (
+                    <p className={correctAnswer ? styles.correctAnswer : styles.incorrectAnswer}>
+                      {correctAnswer ? "Resposta correta" : "Resposta incorreta"}
+                    </p>
+                  )}
+                </div>
+              )}
             </div>
-        </div>
-    )
+            <div className={styles.inputField}>
+              <label htmlFor="word-input">Digite a palavra do áudio</label>
+              <input id="word-input" className={styles.input} onChange={(e) => setWord(e.target.value)} type="text" required />
+            </div>
+            <button className={styles.primaryButton} onClick={handleVerify}>
+              Verificar resposta
+            </button>
+            <p className={styles.paragraph}>
+              {audioPairs.length > 0 ? currentPairIndex + 1 : currentPairIndex} de {audioPairs.length}
+            </p>
+            <div className={styles.buttonsArea}>
+              <button className={styles.secondaryButton} onClick={decreaseIndex} disabled={currentPairIndex === 0}>
+                Anterior
+              </button>
+              <button className={styles.secondaryButton} onClick={increaseIndex} disabled={currentPairIndex === audioPairs.length - 1 || !answered}>
+                Próximo
+              </button>
+              {currentPairIndex === audioPairs.length - 1 && (
+                <button className={styles.secondaryButton} onClick={handleFinalize}>
+                  Finalizar
+                </button>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className={styles.messageArea}>
+            <p className={styles.errorMessage}>{errorMessage}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
-
+ 
 export default Exam
