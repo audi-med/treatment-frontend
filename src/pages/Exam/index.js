0b9f5364545860cebafa532d243e316e87a40962
felipe-coletti <felipecoletti2@gmail.com>
2023-11-21 10:18:07 -0300

    Remoção de um espaço desnecessário

--- src/pages/Exam/index.js
+++ src/pages/Exam/index.js
@@ -56,7 +56,7 @@ const Exam = () => {
             setAnswered(false)
         }
     }
- 
+
     const handleFinalize = async () => {
         try {
             const response = await fetch("http://localhost:8080/api/v1/tratamento/resultado", {
