1ef775a0e3971d00a223790a8dcfafc52ef70e66
felipe-coletti <felipecoletti2@gmail.com>
2023-11-22 19:05:09 -0300

    Remoção de partes desnecessárias

--- src/pages/Patient/index.js
+++ src/pages/Patient/index.js
@@ -1,29 +1,7 @@
-import { React, useState, useEffect } from "react";
+import React from "react";
 import styles from "./styles.module.css";
-import { useParams } from "react-router-dom";
 
 const Patient = () => {
-    const [patient, setPatient] = useState([]);
-    const [errorMessage, setErrorMessage] = useState(null);
-
-    const {id} = useParams();
-    
-    useEffect(() => {
-        const consult = async () => {
-            try {
-                const response = await fetch("http://localhost:8080/api/v1/pacientes/" + id)
-                if (!response.ok) {
-                    throw new Error()
-                }
-                const data = await response.json()
-                setPatient(data)
-            } catch (error) {
-                setErrorMessage("Erro ao exibir os dados do paciente.")
-            }
-        }
-        consult()
-    }, [id])
-
     return (
         <div className={styles.container}>
             <div className={styles.contentArea}>
