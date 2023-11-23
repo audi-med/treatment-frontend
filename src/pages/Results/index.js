81346c240d115d41a350df5e1ee8e05e78b93406
felipe-coletti <felipecoletti2@gmail.com>
2023-11-22 21:17:02 -0300

    Adição do gráfico de resultados

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,17 +1,34 @@
 import { React, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import { Icon } from '@iconify/react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Label, Tooltip } from 'recharts';
  
 const Results = () => {
+    /*const [results, setResults] = useState([])*/
     const [errorMessage, setErrorMessage] = useState(null)
-    
-    const data = [{name: 'Page A', uv: 400}, {name: 'Page B', uv: 200}]
+
+    /*useEffect(() => {
+        const consult = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/tratamento/resultados")
+                if (!response.ok) {
+                    throw new Error()
+                }
+                const data = await response.json()
+                setResults(data)
+            } catch (error) {
+                setErrorMessage("Erro ao exibir os resultados do paciente.")
+            }
+        }
+        consult()
+    }, [])*/
+
+    const results = [{result: 75, date: "02/05/2023"}, {result: 83, date: "03/05/2023"}, {result: 91, date: "04/05/2023"}]
 
     return (
         <div className={styles.container}>
             <div className={styles.contentArea}>
-                <div>
+                <div className={styles.headerArea}>
                     <div className={styles.topArea}>
                         <a className={styles.button} href="/patient">
                             <Icon icon="ep:arrow-left" />
@@ -20,13 +37,20 @@ const Results = () => {
                     <h1 className={styles.primaryTitle}>Resultados</h1>
                 </div>
                 {errorMessage === null ? (
-                    <div className={styles.dataArea}>
-                        <LineChart width={600} height={300} data={data}>
-                            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                            <CartesianGrid stroke="#ccc" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                        </LineChart>
+                    <div className={styles.chartArea}>
+                        <ResponsiveContainer width="100%" height={400}>
+                            <LineChart data={results}>
+                                <Line name="Porcentagem de acertos" type="monotone" dataKey="result" stroke="#8884d8" unit="%" />
+                                <CartesianGrid stroke="#ccc" />
+                                <XAxis dataKey="date">
+                                    <Label value="Data" offset={0} position="insideBottom" />
+                                </XAxis>
+                                <YAxis unit="%">
+                                    <Label value="Porcentagem de acertos" angle={-90} position="insideLeft" />
+                                </YAxis>
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
                     </div>
                 ) : (
                     <div className={styles.messageArea}>
